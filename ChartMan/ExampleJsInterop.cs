78a0eca1a06eff2bfda775f7ed604df4672df799
djembe-waka <jamesboo8500@gmail.com>
2019-03-11 17:12:38 +0800

    Upgrade to Blazor0.9
    Upgrade to Blazor0.9

--- ChartMan/ExampleJsInterop.cs
+++ ChartMan/ExampleJsInterop.cs
@@ -5,21 +5,26 @@ namespace ChartMan
 {
     public class ExampleJsInterop
     {
+        /*
         public static Task<string> Prompt(string message)
         {
             // Implemented in exampleJsInterop.js
+            
             return JSRuntime.Current.InvokeAsync<string>(
                 "exampleJsFunctions.showPrompt",
                 message);
-
+                
         }
+        
         public static Task<string> Display(string message)
         {
+            
             return JSRuntime.Current.InvokeAsync<string>(
                 "JsFunctions.printWorld",
                 message);
-
+                
         }
+        */
    
     }
 }
